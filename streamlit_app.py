ed2fc2797be6f214bc1b5edaab24596b8c00667a
MCompRETools <49099513+MCompRETools@users.noreply.github.com>
2026-08-22 12:26:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1396,6 +1396,10 @@ elif selected_page == "Sustainability Knowledge":
                 "Select Gemini Model",
 
                 [
+                    "gemini-3.1-pro-preview",
+                    "gemini-3.5-flash-lite",
+                    "gemini-3.6-flash",
+                    "gemini-3.7-flash",
                     "gemini-2.5-flash",
                     "gemini-1.5-pro",
                     "gemini-1.5-flash"
